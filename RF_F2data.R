97b73cbc004aae3a6a4ca347a6fa7ff3e134ab44
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-06-05 09:16:51 -0400

    Update RF_F2data.R

--- RF_F2data.R
+++ RF_F2data.R
@@ -15,7 +15,7 @@ testingset <- trainingdata[-train_index, ]
 
 ## create cross validation strategy ##
 control <- trainControl(method='repeatedcv', 
-                        number=5, ##will test 10 different values for mtry (number of variables for splitting) ##
+                        number=5, ##will test 5 different values for mtry (number of variables for splitting) ##
                         repeats=1,
                         search = "random")  
 
