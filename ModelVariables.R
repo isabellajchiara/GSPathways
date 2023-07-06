7d88a68153a26899947e8bebc0044b1c99910b0c
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2023-07-06 11:06:43 -0300

    Change library to calculate cholesky

--- ModelVariables.R
+++ ModelVariables.R
@@ -41,7 +41,7 @@ if (args$model == "svm"){
 
 if (args$model == "ann"){
     fileTrain <- "ANN_RD.R"
-    modelLibs <- c("tidyverse","keras","tensorflow","readr","devtools")
+    modelLibs <- c("tidyverse","keras","tensorflow","readr","devtools","BNSP")
     hasParallelVersion <- FALSE
 
     getEBV <- function(gen){
