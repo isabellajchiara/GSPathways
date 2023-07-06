bff3806e0eaf180b90e3c68f1ecdb257dd1d7622
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2023-07-06 10:22:07 -0300

    add devtools

--- ModelVariables.R
+++ ModelVariables.R
@@ -41,11 +41,11 @@ if (args$model == "svm"){
 
 if (args$model == "ann"){
     fileTrain <- "ANN_RD.R"
-    modelLibs <- c("tidyverse","keras","tensorflow","readr","BMTME")
+    modelLibs <- c("tidyverse","keras","tensorflow","readr","BMTME","devtools")
     hasParallelVersion <- FALSE
 
     getEBV <- function(gen){
         PYTgeno <- as.matrix(pullSegSiteGeno(PYT))
-        model_Final  %>% predict(PYTgeno)
+        model_Final %>% predict(PYTgeno)
     }
 }
