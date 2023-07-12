84c7311ebd85b5b0a353ca310c50ef27b81db3fa
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-07-12 11:23:42 -0400

    Update RF_SC.R

--- models/RF_SC.R
+++ models/RF_SC.R
@@ -9,54 +9,7 @@ library(factoextra)
 y <- as.data.frame(TrainingPheno)
 M <- as.data.frame(TrainingGeno)
 
-newgeno <- M %>%  select(where(~ n_distinct(.) > 1))
-
-colnames(newgeno) =NULL
-
-PCAgeno <- prcomp(newgeno, center=TRUE, scale=TRUE) ##take out categorical columns##
-
-PCAselected = as.data.frame(-PCAgeno$x[,1:3])
-
-silhouette <- fviz_nbclust(PCAselected, kmeans, method = 'silhouette')
-kvalues <- silhouette$data ##largest value tells how many clusters are optimal ##
-kvalues <- kvalues[order(-kvalues$y),]
-
-k=as.numeric(kvalues[1,1])
-
-kmeans_geno = kmeans(PCAselected, centers = k, nstart = 50)
-clusters <- fviz_cluster(kmeans_geno, data = PCAselected)
-
-clusterData <- clusters$data
-
-clusterData <- clusterData[order(clusterData$cluster),]
-
-nclusters <- as.numeric(clusterData[as.numeric(nrow(clusterData)),as.numeric(ncol(clusterData))])
-
-i = 1
-datalist = vector("list", length = nclusters)
-for (i in 1:nclusters) {
-  clustername <- paste0("cluster",i)
-  clustername <- clusterData[clusterData$cluster==i,] 
-  
-  assign(paste0("cluster",i), clustername)
-  
-  trnname <- paste0("trn",i)
-  trnname <- clustername[sample(0.3*nrow(clustername)),]
-  datalist[[i]] <- trnname
-
-  i = i + 1
-  if (i > nclusters){ 
-    break
-  }
-  }
-  
-TRN <- do.call(rbind, datalist)
-
-TRN <- TRN[,1]
-
-OptimGeno <- M[TRN,]
-y <- as.data.frame(y)
-OptimPheno <- y[TRN,]
+StratClusTRN(y,M) 
 
 BV <- OptimPheno
 
