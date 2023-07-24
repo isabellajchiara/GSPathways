8e1e2d0d7f79ffc033aeba0d32a2d59778f99255
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-07-24 13:48:22 -0400

    Create readVariances.R

---
+++ DataVisualization/readVariances.R
@@ -0,0 +1,17 @@
+nCycle = 3
+datalist = list()
+
+#gather data from all consecutive cycles
+for (cycle in 1:nCycle) {
+  filename = paste("C", cycle, "_rrblup_random_F2_F5_F2_cors_snp_yield.csv", sep="")
+  data = read.csv(filename)
+  datalist[[cycle]] = data
+}
+varianceValues <- do.call(rbind, datalist)
+varianceValues = varianceValues[ , colSums(is.na(varianceValues))==0] #remove columns(reps) containing an NA
+
+# take the mean across reps 
+values = as.data.frame(varianceValues[,-1])
+means = rowMeans(values)
+gens = as.data.frame(varianceValues[,1])
+resultsVARs = cbind(gens,means)
