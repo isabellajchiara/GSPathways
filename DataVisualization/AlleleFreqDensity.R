edab06f411dd4aae13bc023d3d7195128e82000a
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-07-26 10:11:17 -0400

    check and add clarity

--- DataVisualization/AlleleFreqDensity.R
+++ DataVisualization/AlleleFreqDensity.R
@@ -10,91 +10,92 @@ for (cycle in 1:ncycles){
   for (i in 1:nreps) {
     for(gen in genList){
       
-      filename = paste("1C", cycle, "_svm_random_alleles_snp_yield.rds", sep="")
+      filename = paste("C", cycle, "_rrblup_random_F2_F5_F2_alleles_snp_yield.rds", sep="")
       genotypes <- readRDS(filename) #read in each cycle's SNP data
       
       repMat <- genotypes[[i]] #pull out one rep
       genMat1 <- repMat[repMat$Gen==gen,] # pull out one generation
       genMat2 <- genMat1[,-1] # remove label column
       alleleSum <- as.matrix(colSums(genMat2)) # sum copy of alleles coded 1
-      alleleFreq <- as.data.frame(alleleSum/nrow(genMat2)) # divide by number of alleles total
+      x <- as.matrix(alleleSum/nrow(genMat2)) # divide by number of alleles total
       
-      Major <- as.matrix(ifelse(x<=0.5, 1-x, x*1)) #if the frequency is less than half, then the other allele is major and that is the Freq we want
+      Major <- as.matrix(ifelse(x<=0.5, 1-x, x*1)) #if the frequency is less than 0.5, then the other allele is major so we will calculate 1-x to make all Freqs the Major Freq
       
-      freqList[[gen]] <- Major
+      freqList[[gen]] <- Major #add generation frequency to the freqList
+      #cycle through all gens for a given rep
     }
-    freqDF <- do.call(cbind, freqList)
-    freqDF <- freqDF[,-1]
-    datalist[[i]] <- freqList
+    freqDF <- do.call(cbind, freqList) #turn list into DF
+    datalist[[i]] <- freqList #add the freqList for 1 rep to the dataList
   }
-  datalistDF = as.data.frame(do.call("rbind",datalist))
-  saveRDS(datalist,paste("datalistC",cycle,".rds", sep=""))
+  datalistDF = as.data.frame(do.call("rbind",datalist)) # turn to DF
+  saveRDS(datalist,paste("datalistC",cycle,".rds", sep="")) #we will have one dataList for each cycle
 }
 
 
-# datalist is a list of data frames
+# datalist is a list of data frames. there is one datalist for each cycle. 
 # each data frame represents one rep
-# each column represents a generation
+# each column in a data frame represents a generation
 # each row represents the frequency of each SNP
 
 #next we need the mean frequency across reps 
 
-MeanFreq = matrix(nrow=nSNP, ncol=length(genList)
+MeanFreq = matrix(nrow=nSNP, ncol=length(genList)) 
 AllData = list()
 
 for (cycle in 1:ncycles) {
-  for (gen in (1:length(genList))){
-    datafile = paste("datalistC",cycle,".rds", sep="")
-    DF = readRDS(datafile)
-    DF = t(do.call(cbind,DF))
-    colnames(DF) = c(1:length(genList))
-    genDF <- DF[,gen]
-    genDF1 <- do.call("cbind",genDF)
-    genMeans <- as.matrix(rowMeans(genDF1))
-    MeanFreq[,gen] = genMeans
-  }
+    for (gen in (1:length(genList))){
+    datafile = paste("datalistC",cycle,".rds", sep="") #read in the datalist file for each cycle
+    DF = readRDS(datafile) 
+    DF = t(do.call(cbind,DF)) # turn to DF with each generation (all reps) representing 1 column
+    colnames(DF) = c(1:length(genList)) # number the columns
+    genDF <- DF[,gen] #pull out one generation from the cycle
+    genDF1 <- do.call("cbind",genDF) # turn to DF 
+    genMeans <- as.matrix(rowMeans(genDF1)) #find the mean frequency across all reps 
+    MeanFreq[,gen] = genMeans # add to the MeanFreq matrix
+    }
   assign(paste0("meanFreqsC",cycle),MeanFreq) 
   saveRDS(MeanFreq,paste("MeanFreqC",cycle,".rds",sep=""))
-} 
+  } 
+
 
 FinalAlleleFreq =list()
 
 for (x in 1:ncycles){
   data = paste0("meanFreqC",cycle, ".rds")
-  data = as.data.frame(readRDS(data))
-  FinalAlleleFreq[[x]] = data
+  data = as.data.frame(readRDS(data))# read in the mean freqs for each cycle 
+  FinalAlleleFreq[[x]] = data # accumulate them in the FinalAlleleFreq list
 }
 
-Alldata = do.call("cbind",FinalAlleleFreq)
+Alldata = do.call("cbind",FinalAlleleFreq) # now we have a data frame with the mean freq for reps for each consecutive gen
 colnames(Alldata) = c(rep(genList, times=ncycles))
                       
 ###
-BOC <- as.data.frame(Alldata[,1])
+BOC <- as.data.frame(Alldata[,1]) # take the fist gen of the cycle
 BOC$base <- rep(c("C1"), times = nrow(BOC))
 colnames(BOC) <- c("freq","stage")
                       
-MOC <- as.data.frame(Alldata[,ncol(Alldata*0.5)])
+MOC <- as.data.frame(Alldata[,ncol(Alldata*0.5)]) #take the middle of the 3 cycles 
 MOC$base <- rep(c("C2"), times = nrow(MOC))
 colnames(MOC) <- c("freq","stage")
                       
-EOC <- as.data.frame(Alldata[,ncol(Alldata)])
+EOC <- as.data.frame(Alldata[,ncol(Alldata)]) # take the last gen in the cycle 
 EOC$base <- rep(c("C3"), times = nrow(EOC))
 colnames(EOC) <- c("freq","stage")
                       
-alldata <- rbind(BOC,MOC,EOC)
+alldata <- rbind(BOC,MOC,EOC) # we will look at just these 3 gens for simplicity
                       
     
 library(ggplot2)
 library(lattice)
 
 # Density plot with semi-transparent fill
-p<-ggplot(alldata, aes(x=freq, fill=stage)) +
+p<-ggplot(alldata, aes(x=freq, fill=stage)) + #plot the frequency and color by stage in the cycle 
           geom_density(alpha=0.5)
 
 p
                       
-# calcjulate overlap proportion
-x <- list(C1=MOC$freq, C3=EOC$freq)
+# calculate overlap proportion
+x <- list(C1=MOC$freq, C3=EOC$freq) # calculate the overlap between any 2stages 
                     
 
 overlap(x, plot=FALSE, type="2")                      
--- DataVisualization/confusionMatrixBVEBV.R
+++ DataVisualization/confusionMatrixBVEBV.R
@@ -29,7 +29,7 @@ for (cycle in 1:nCycle) {
   data2 <- data2[order(data2$ebv),]
   
   ## assign bv categories to each obs the data  
-  cutoffBV =    data1[nrow(data1)*0.8,2]
+  cutoffBV =    data1[nrow(data1)*0.9,2]
   data1$bvCat <- as.factor(ifelse(data1$bv > cutoffBV, "1", "0"))
                 
   ## put the data back in its original order
@@ -37,7 +37,7 @@ for (cycle in 1:nCycle) {
   bvCat <- as.factor(data1Reordered$bvCat)
 
   # assign ebv catagories to each obs in the data
-  cutoffEBV = data2[nrow(data2)*0.8,3]
+  cutoffEBV = data2[nrow(data2)*0.9,3]
   data2$ebvCat <- as.factor(ifelse(data2$ebv > cutoffEBV, "1", "0"))
   
   
@@ -48,16 +48,16 @@ for (cycle in 1:nCycle) {
   # calculate CM
   CM <- confusionMatrix(data=bvCat, reference = ebvCat) # the CM is for ONE GENERATION OF ONE REP
   
-  Acc <- as.data.frame(CM$overall)
+  Acc <- as.data.frame(CM$overall) # pull accuracy
   Accuracy <- Acc[1,1]
   SensSpec <- as.data.frame(CM$byClass)
-  Sensitivity <- SensSpec[1,1]
-  Specificity <- SensSpec[2,1]
+  Sensitivity <- SensSpec[1,1] #pull sensitivity
+  Specificity <- SensSpec[2,1] #pull specificity
   
-  df <-cbind(Accuracy, Sensitivity, Specificity)
-  rownames(df) = gen
-  datalist[[x]] = df
-  x=x+1
+  df <-cbind(Accuracy, Sensitivity, Specificity) #bind values 
+  rownames(df) = gen 
+  datalist[[x]] = df #accumulate values 
+  x=x+1 #move on to the next set of values 
 
   }
 }  
@@ -70,16 +70,19 @@ reps = 15
 r = 1
 cmMeans = list()
 while (reps <= nrow(cmValues)) {
-  set = as.matrix(cmValues[(reps-(reps-1)):reps,])
-  repMean = colMeans(set)
-  cmMeans[[r]] = repMean
+  set = as.matrix(cmValues[(reps-(reps-1)):reps,]) # pull out the first 15 obs (each rep for a given generation)
+  repMean = colMeans(set) # find the means 
+  cmMeans[[r]] = repMean # collect the means 
   r = r +1
-  reps = 15 * r
+  reps = 15 * r #change 15 to number of reps if number of reps is not 15. CMda
 }
 
-CMdata = do.call(rbind, cmMeans)
-rownames(CMdata) = rep(genList,3)
+CMdata = do.call(rbind, cmMeans) # create DF
+rownames(CMdata) = rep(genList,3) # name the generations 
 
+
+#The results of the CM will tell us how well we were able to correctly identify the top 10% of individuals as top
+#and the bottom 90% of individuals as bottom
 
 
 
