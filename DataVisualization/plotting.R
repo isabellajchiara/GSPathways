732399870468145cddefb7ccd22164da8a051fff
Hoi Ching Or <orhc225@gmail.com>
2023-07-28 09:03:06 -0400

    Create loop to bind correlations dataframe

--- DataVisualization/plotting.R
+++ DataVisualization/plotting.R
@@ -1,10 +1,24 @@
 library(ggplot2)
 
-# correlation line plot
-data_cors <- read.csv("sim_2023Jul18_101500/1C1_svm_random_cors_snp_yield.csv")
-data_cors$X <- factor(data_cors$X, levels = data_cors$X)
+nCycle <- 3
+data_cors <- data.frame()
 
-plot_cors <- ggplot(data_cors, aes(x = X, y = X1, group = 1)) +
-            geom_line() +
-            geom_point()
-ggsave("CorrelationLinePlot.pdf", plot = plot_cors)
+# correlation line plot
+for (cycle in 1:nCycle) {
+    if (cycle == 1) {
+        data_cors <- read.csv(paste("sim_2023Jul18_101500/1C", cycle, "_svm_random_cors_snp_yield.csv", sep = ""))
+        colnames(data_cors)[cycle + 1] <- paste("1C", cycle, sep = "")
+    }else {
+        temp_data <- read.csv(paste("sim_2023Jul18_101500/1C", cycle, "_svm_random_cors_snp_yield.csv", sep = ""))
+        data_cors <- cbind(data_cors, temp_data$X1)
+        colnames(data_cors)[cycle + 1] <- paste("1C", cycle, sep = "")
+    }
+}
+# data_cors <- read.csv("sim_2023Jul18_101500/1C1_svm_random_cors_snp_yield.csv")
+data_cors$X <- factor(data_cors$X, levels = data_cors$X)
+data_cors
+
+# plot_cors <- ggplot(data_cors, aes(x = X, y = X1, group = 1)) +
+#             geom_line() +
+#             geom_point()
+# ggsave("CorrelationLinePlot.pdf", plot = plot_cors)
