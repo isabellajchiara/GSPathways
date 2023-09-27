08eec7bc1e6e61a288689c1ac082f6531442e931
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-09-27 13:35:15 -0400

    Update ANN_RD.R

--- models/ANN_RD.R
+++ models/ANN_RD.R
@@ -2,6 +2,30 @@ library(tidyverse)
 library(keras)
 library(readr)
 
+## create GRM ##
+geno <- as.matrix(M)
+GM <- tcrossprod(geno)/dim(geno)
+LG <- GM
+
+Y <- as.matrix(y)
+X = LG
+
+X <- as.matrix(M)
+Y <- as.matrix(y)
+
+train_index <- sample(1:nrow(Y), 0.75 * nrow(Y))
+X_train <- X[train_index, ]
+Y_train <- Y[train_index,]
+
+X_test <- X[-train_index, ]
+Y_test <- Y[-train_index, ]
+
+## define input and number of epochs ##
+inputs = layer_input(shape=(ncol(X_train))) 
+nEpoch = 100
+batchSize = 100
+valSplit = 0.2
+
 
 # add layers
 predictions <- inputs %>% 
