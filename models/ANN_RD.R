eadce79eed45d4014e26339da975295765cecb9c
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-09-27 17:19:54 +0000

    update ann fitting

--- models/ANN_RD.R
+++ models/ANN_RD.R
@@ -2,30 +2,6 @@ library(tidyverse)
 library(keras)
 library(readr)
 
-## create GRM ##
-geno <- as.matrix(M)
-GM <- tcrossprod(geno)/dim(geno)
-LG <- GM
-
-Y <- as.matrix(y)
-X = LG
-
-X <- as.matrix(M)
-Y <- as.matrix(y)
-
-train_index <- sample(1:nrow(Y), 0.75 * nrow(Y))
-X_train <- X[train_index, ]
-Y_train <- Y[train_index,]
-
-X_test <- X[-train_index, ]
-Y_test <- Y[-train_index, ]
-
-
-## define input and number of epochs ##
-inputs = layer_input(shape=(ncol(X_train))) 
-nEpoch = 100
-batchSize = 100
-valSplit = 0.2
 
 # add layers
 predictions <- inputs %>% 
@@ -37,7 +13,7 @@ predictions <- inputs %>%
 model <- keras_model(inputs = inputs, outputs = predictions) 
 model %>% compile( 
   optimizer = 'rmsprop', 
-  loss = 'binary_crossentropy',
+  loss = 'sparse_categorical_crossentropy',
   metrics = c('accuracy')
 ) 
 
