95068acba32dda8b6048adeb0ea682d777922508
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-10-02 15:23:30 -0400

    Create GenotypesPerGen.R

---
+++ DataVisualization/GenotypesPerGen.R
@@ -0,0 +1,38 @@
+# depending on ncycles and nreps, this can be a very slow script
+# consider running in the cluster if your machine doesn't have a ton of memory
+
+cycle = 1
+nreps=15
+nSNP = 3547
+gen = "PYT"
+
+allelelist <- list()
+datalist <- list()
+
+for (i in 1:nreps) {
+      filename = paste("C", cycle, "_ann_random_trainAtF5_trainWithF2_F2Parents_alleles_snp_yield.rds", sep="")
+      genotypes <- readRDS(filename) #read in each cycle's SNP data
+      repMat <- genotypes[[i]] #pull out one rep
+      genMat1 <- repMat[repMat$Gen==gen,] # pull out one generation
+      genMat2 <- genMat1[,-1] # remove label column
+      allelelist[[i]] <- genMat2 #add generation frequency to the allelelist
+      cat("finished rep", i,"of", nreps, "for cycle", cycle,'\n')
+}
+
+repMode = matrix(nrow=120,ncol=nSNP)
+nrow = 120
+ncol = nSNP
+
+  for (x in 1:nrow){
+    for (y in 1:ncol){
+      for (z in 1:nreps){
+        rep = allelelist[[z]] 
+        cellVals = matrix(nrow=1, ncol=nreps)
+        cellVals[[y]] = rep[x,y]
+        mode = which.max(cellVals)
+        repMode[x,y] = mode }
+    }
+  }
+
+        
+        
