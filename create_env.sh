630434e504d1f9f81c69e4a51a6ecfa358d617bd
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-10-09 08:04:59 -0400

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,13 +1,14 @@
+# first time creating the venv
 
-#module load python gcc r/4.2.2
-#virtualenv --no-download venv
-#source venv/bin/activate
+module load python gcc r/4.2.2
+virtualenv --no-download venv
+source venv/bin/activate
 
-#pip install --no-index --upgrade pip
-#pip install keras
-#pip install tensorflow
+pip install --no-index --upgrade pip
+pip install keras
+pip install tensorflow
 
-#deactivate
+deactivate
 
 ### subsequent sessions
 module load python gcc r/4.2.2
