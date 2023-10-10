54853dd0b5fbe8540564127c3ed5260fa37948c1
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-10-10 08:35:14 -0400

    Update and rename create_env.sh to call_env.sh

---
+++ call_env.sh
@@ -0,0 +1,13 @@
+
+
+### subsequent sessions after your first run using create_env.sh
+module load python gcc r/4.2.2
+source venv/bin/activate
+cd projects
+cd def-haricots
+cd ich
+cd MultipleCycles-main
+R
+
+
+
