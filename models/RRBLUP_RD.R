b78136d878dc0a91750c0caa925f2e61b6fb8fcf
Isabella C. Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-10-26 20:32:48 -0400

    Update RRBLUP_RD.R

--- models/RRBLUP_RD.R
+++ models/RRBLUP_RD.R
@@ -5,8 +5,10 @@ trainIndex <- as.matrix(sample(1:nrow(M)), 0.75*(nrow(M)))
 phenoTrain <- y[trainIndex,]
 genoTrain <- M[trainIndex,]
 
+GM=tcrossprod(genoTrain)/dim(genoTrain)
+
 BV <- phenoTrain
 
-EBVans <-mixed.solve(BV, Z=genoTrain, K=NULL, SE=FALSE, return.Hinv=FALSE)
+EBVans <-mixed.solve(BV, Z=GM, K=NULL, SE=FALSE, return.Hinv=FALSE)
 markerEffects <- EBVans$u
 markerEffects <- as.vector(markerEffects)
