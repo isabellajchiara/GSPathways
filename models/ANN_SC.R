a6455ffa21b4e9dca33e5a1b26814b16a5012622
Isabella C. Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-11-04 12:25:14 -0400

    update model parameters and add training matrices

--- models/ANN_SC.R
+++ models/ANN_SC.R
@@ -29,98 +29,56 @@ M <- as.data.frame(M)
 
 StratClusTRN(y,M) #calls function for stratified clustering algorithm
 
-BV <- OptimPheno
+Y <- as.data.frame(OptimPheno)
+X <- as.data.frame(OptimGeno)
 
-trainingset= as.data.frame(cbind(BV,OptimGeno))
-colnames(trainingset) <- paste("ID",1:(ncol(y) + ncol(M)), sep="")
+train_index <- sample(1:nrow(Y), 0.75 * nrow(Y))
+X_train <- as.matrix(X[train_index, ])
+Y_train <- as.matrix(Y[train_index,])
+
+X_test <- as.matrix(X[-train_index, ])
+Y_test <- as.matrix(Y[-train_index, ])
 
 
 ## one holdout CV ##
 
-No_Epoch = 1000
-N_Units = 33
-X_trn = M[TRN,]
-X_tst = M[-TRN,]
-Y_trn = Y[TRN,]
-Y_tst = Y[-TRN,]
+nEpoch = 100
+batchSize = 100
+valSplit = 0.2
 
-## Build model using pipe operator ##
 
-build_model <- function() {
-  model <- keras_model_sequential()
-  model %>%
-    layer_dense(units =N_Units, activation = "relu", input_shape = c(dim
-                                                                     (X_trn)[2])) %>%
-    layer_dropout(rate = 0.1) %>%
-    layer_dense(units = 1, activation = "linear")
-  model %>% compile(
-    loss = "mse",
-    optimizer = "rmsprop",
-    metrics = c("mse"))
-  model}
+# add layers
 
-## build and view model ##
+inputs = layer_input(shape=(ncol(X_train))) 
 
-model <- build_model()
-model %>% summary()
+predictions <- inputs %>% 
+  layer_dense(units = ncol(X_train), activation = 'relu') %>% 
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 1)
 
-## fitting and plotting the model ##
+# create and compile model 
 
-print_dot_callback <- callback_lambda(
-  on_epoch_end = function(epoch, logs) {
-    if (epoch %% 20 == 0) cat("\n")
-    cat(".")
-  })
+model <- keras_model(inputs = inputs, outputs = predictions) 
 
-## inner training and validation ##
+earlyStopping <- callback_early_stopping(monitor = "mean_squared_error", min_delta = 0.1, patience = 10,
+                                         mode = "auto")
 
-model_fit <- model %>% fit(
-  X_trn, Y_trn,
-  shuffle=F,
-  epochs = No_Epoch, batch_size = 640,
-  validation_split = 0.2,
+model %>% compile( 
+  optimizer = 'Adam', 
+  loss = 'mean_squared_error',
+  metrics = c('mean_squared_error')
+) 
+
+fit(
+  model,
+  x = X_train,
+  y = Y_train,
+  batch_size = batchSize,
+  epochs = nEpoch,
   verbose = 0,
-  callbacks = list(print_dot_callback))
-
-## plot ## 
-
-plot(model_fit)
-
-## Refit model using early stopping ##
-
-early_stop <- callback_early_stopping(monitor="val_loss", mode='min', patience =50)
-
-model_Final <- build_model()
-model_fit_Final <- model_Final%>%fit(
-  X_trn, Y_trn,
-  shuffle=F,
-  epochs = No_Epoch, batch_size=640,
-  validation_split = 0.2,
-  verbose=0, callbacks = list(early_stop, print_dot_callback)
+  validation_split = valSplit,
+  callbacks = list(earlyStopping)
 )
 
-## plot history of training process ##
 
-length(model_fit_Final$metrics$mean_squared_error)
-plot(model_fit_Final)
 
-## predition to see how model performs##
-
-prediction = model_Final %>% predict(X_tst)
-Predicted = c(prediction)
-Observed = Y_tst
-plot(Observed, Predicted)
-MSE = mean((Observed-Predicted)^2)
-MSE
-Obs_Pred = cbind(Observed, Predicted)
-colnames(Obs_Pred) = c("Observed", "Predicted")
-Obs_Pred
-
-###### Model is built, continue with breeding simulation #####
-
-## Use model to predict F2 EBV ##
-
-geno <- pullSnpGeno(F2)
-geno <- as.matrix(geno)
-GM <- tcrossprod(geno)/dim(geno)
-LGF2 <- cholesky(GM)
