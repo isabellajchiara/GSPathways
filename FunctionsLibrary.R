2c53cb13cc6ec1fa1f8ff15ced0ad46090d7b467
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-12-08 08:58:57 -0500

    Update FunctionsLibrary.R

--- FunctionsLibrary.R
+++ FunctionsLibrary.R
@@ -134,6 +134,14 @@ getVariances <- function(variances){
   variances
 }
 
+getPheno <- function(pheno){
+  pheno <- as.data.frame(pheno)
+  colnames(pheno) <- c(1:args$nReps)
+  rownames(pheno) <- c("PrevCycPYT", "newParents","F1","F2", "F3","F4", "F5", "PYT","AYT","Variety")
+  variances
+}
+
+
 # use trainGen to retrain the model
 trainModel <- function(){
 
