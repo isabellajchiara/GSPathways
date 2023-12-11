cc4788a10b8ab7f672bcdcb0d1a82d747528db95
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-12-11 12:16:11 -0500

    rrblup alert

--- models/RRBLUP_RD.R
+++ models/RRBLUP_RD.R
@@ -1,8 +1,11 @@
 
 
+
 phenoTrain <- as.matrix(y)
 genoTrain <- as.matrix(M)
 
 EBVans <-mixed.solve(phenoTrain, Z=genoTrain, K=NULL, SE=FALSE, return.Hinv=FALSE)
 markerEffects <- EBVans$u
 markerEffects <- as.vector(markerEffects)
+
+cli_alert_success("Calculated marker effects at {args$trainGen} using {args$trainingData} data")
