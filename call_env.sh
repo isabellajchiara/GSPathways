9f9039f40c42cca78684f33508183a0ddb15faec
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-01-17 08:27:31 -0500

    Update call_env.sh

--- call_env.sh
+++ call_env.sh
@@ -1,12 +1,13 @@
 
 
 ### subsequent sessions after your first run using create_env.sh
+salloc --time=1:30:00 --mem=90G --cpus-per-task=15
 module load python gcc r/4.2.2
 source venv/bin/activate
 cd projects
 cd def-haricots
 cd ich
-cd MultipleCycles-main
+cd MultipleCycles-Run
 R
 
 
