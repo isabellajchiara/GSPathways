3098c65c65e739e93a6d8fc2210b896b38ca201c
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-02-01 10:09:36 -0500

    Update call_env.sh

--- call_env.sh
+++ call_env.sh
@@ -5,12 +5,11 @@ module load python gcc r/4.2.2
 source venv/bin/activate
 
 salloc --time=1:30:00 --mem=90G --cpus-per-task=15
+module load r/4.2.2
 cd projects
 cd def-haricots
 cd ich
 cd MultipleCycles-Run
-module load r/4.2.22
-
 R
 source("RunSims.R")
 
