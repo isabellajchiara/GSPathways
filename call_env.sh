00bfd872055b9c29ed3890921a5b8416c339389d
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-04-25 10:56:06 -0400

    Update call_env.sh

--- call_env.sh
+++ call_env.sh
@@ -4,8 +4,7 @@
 module load python gcc r/4.2.2
 source venv/bin/activate
 
-salloc --time=2:30:00 --mem=200G --cpus-per-task=15
-module load r/4.2.2
+salloc --time=1:30:00 --mem=200G --cpus-per-task=4
 cd projects
 cd def-haricots
 cd ich
